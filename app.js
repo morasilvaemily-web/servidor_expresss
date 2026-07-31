a463f5747886d86a059f2595269ec4a950f2dd18
emily <morasilvaemily@gmail.com>
2026-07-31 16:35:47 -0500

    parametros

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 import express from 'express';
-import{configDotenv} from "dotenv"
-configDotenv();
+import "dotenv/config"
 const app = express();
 const port = process.env.puerto|| 3000;
 app.get("/", (_, res) => {
-res.send('Aprendicez ficha 3407186');
+res.send('Aprendicez ficha 3407186 SENA');
 });
-app.listen(port, () => {
-console.log( `servidor: http//localhost:${port}`);
+
+//endpoint
+app.get("/ruta1", (req, res) => {
+    res.send('<h1>Usuario res.send</h1>');
+});
+app.get("/ruta2", (req, res) => {
+    res.json({"dev":"node --wath app.js","script":"node app.js"})
+    })
+
+app.get("/ruta3/:nombre/:apellido", (req, res) => {
+     const nameUsuario = req.params.nombre
+     const apellido = req.params.apellido
+    res.json({"usuario" :nameUsuario,"apellido":apellido})
+    })
+
+ app.get("/ruta4", (req, res) =>{
+            const numero = req.query.phone ||3434565476658
+            const orden = req.query.orden|| "sin orden"
+            const pagina =req.query.pagina||1
+    res.send(`<h1>listado aprendices </h1>
+        <h2>el listado en orden:${orden} </h2>
+        <p>pagina: ${pagina}<p>
+    <h3>Numero:${numero}</h3>`)
+    })
+
+
+
+
+
+    app.listen(port, () => {
+console.log( `servidor: http://localhost:${port}`);
 });
