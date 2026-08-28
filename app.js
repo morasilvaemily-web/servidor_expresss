9c35dd1bf5d37b366c37777e808d8bf02cede637
taniacastiblanco208-sketch <taniacastiblanco208@gmail.com>
2026-08-28 16:48:11 -0500

    Aprendices

--- app.js
+++ app.js
@@ -1,9 +1,69 @@
-import express from 'express';
+const express = require('express');
 const app = express();
-const port = 3000;
-app.get("/", (_, res) => {
-res.send('Aprendicez ficha 3407186');
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send('Api rest aprendices');
 });
+
+//middleware para recibir datos en formato json
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//lista de aprendices
+app.get("/api/aprendices", (req, res) => {
+    res.status(200).json({
+        "mensaje":"Lista de aprendices",
+    });
+});
+//listar un aprendiz 
+app.get("/api/aprendices/:id", (req, res) => {
+    res.status(200).json({
+        "mensaje":"Lista 1 aprendiz",
+    });
+});
+
+//empoindt crear aprendiz
+app.post("/api/aprendices/", (req, res) => {
+    res.status(201).json({
+        "mensaje":"crear aprendices",
+    });
+});
+
+//empoindt editar aprendiz
+app.put("/api/aprendices/:id", (req, res) => {
+    res.status(201).json({
+        "mensaje":"editar aprendices",
+    });
+});
+
+//empoindt eliminar aprendiz
+app.delete("/api/aprendices/:id", (req, res) => {
+    res.status(200).json({
+        "mensaje":"eliminar aprendices",
+    });
+});
+
+app.post("/rutaJson",(req, res)=>{
+    const todosDatos = req.body;
+    const todosData= req.body.edad2
+    if(edad>=18){
+        res.json({mensaje:"es mayor"})
+    } else{
+        res.json({mensaje:"es menor"})
+    }
+    res.json({datoJson:todosDatos})
+});
+
+app.post("/rutaFormulario",(req, res)=>{
+const todosDatos= req.body
+const programa = req.body.programa 
+    res.json({TodosDatos: todosDatos, MiPrograma:programa})
+});
+
+
+
 app.listen(port, () => {
-console.log( `Servidor en funcionamiento en el puerto: `);
+    console.log( `Servidor :http://localhost:${port}` );
 });
